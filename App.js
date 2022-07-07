6dc6c24b8d501c9b250d16a3ea43566eef95be22
Jeremy Cabrera <jeremy.cabrera@outlook.com>
2022-07-07 11:18:24 -0400

    Init commit

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
+import React, {useState, useCallback} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, ImageBackground, TextInput, ActivityIndicator } from 'react-native';
+import axios from 'axios';
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  image: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  textInput: {
+    borderBottomWidth: 3,
+    padding: 5,
+    paddingVertical: 20,
+    marginVertical: 100,
+    marginHorizontal: 10,
+    backgroundColor: '#fff',
+    fontSize: 19,
+    borderRadius: 16,
+    borderBottomColor: '#df8e00',
+  }
 });
+
+const App = () => {
+  const [input,setInput] = useState("");
+  const [loading,setLoading] =useState(false);
+  const [data,setData] = useState([]);
+
+  const fetchDataHandler = useCallback(() => {}, []);
+
+
+  return <View style={styles.root}>
+    <ImageBackground 
+    source={require('./assets/clTigers.jpg')}
+    resizeMode="cover"
+    style={styles.image}>
+      <View>
+        <TextInput placeholder='Enter your team'
+        onChangeText={text=>setInput(text)} 
+        value={input}
+        placeholderTextColor={'#fff'}
+        style={styles.textInput}
+        onSubmitEditing={fetchDataHandler}
+        />
+      </View>
+
+
+    </ImageBackground>
+    
+  </View>
+};
+
+export default App;
